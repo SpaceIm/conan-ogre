1082bf07458f00db7884fa2ed095aec7b087db93
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-04-04 19:32:35 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# conan-ogre
+
+## Package Status
+
+| Windows (MinGW) | Windows (Visual Studio) | Linux | macOS |
+|:---------------:|:-----------------------:|:-----:|:-----:|
+|[![Build status](https://ci.appveyor.com/api/projects/status/x7n2i5eteddjodsv/branch/testing%2F1.12.11?svg=true)](https://ci.appveyor.com/project/SpaceIm/conan-ogre)|[![Build status](https://github.com/SpaceIm/conan-ogre/workflows/.github/workflows/windows.yml/badge.svg?branch=testing%2F1.12.11)](https://github.com/SpaceIm/conan-ogre/actions/workflows/windows.yml?query=branch%3Atesting%2F1.12.11)|[![Build status](https://github.com/SpaceIm/conan-ogre/workflows/.github/workflows/linux.yml/badge.svg?branch=testing%2F1.12.11)](https://github.com/SpaceIm/conan-ogre/actions/workflows/linux.yml?query=branch%3Atesting%2F1.12.11)|[![Build status](https://github.com/SpaceIm/conan-ogre/workflows/.github/workflows/macos.yml/badge.svg?branch=testing%2F1.12.11)](https://github.com/SpaceIm/conan-ogre/actions/workflows/macos.yml?query=branch%3Atesting%2F1.12.11)|
+
+## Description
+
+[Conan.io](https://conan.io) package for [OGRE](https://www.ogre3d.org).
---
+++ conanfile.py
@@ -0,0 +1,66 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class OgreConan(ConanFile):
+    name = "ogre"
+    description = "OGRE (Object-Oriented Graphics Rendering Engine) is a scene-oriented, " \
+                  "flexible 3D engine written in C++ designed to make it easier and more " \
+                  "intuitive for developers to produce games and demos utilising 3D hardware."
+    license = "MIT"
+    topics = ("conan", "ogre", "ogre3d", "engine", "rendering")
+    homepage = "https://www.ogre3d.org"
+    url = "https://github.com/conan-io/conan-center-index"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+    }
+
+    exports_sources = "CMakeLists.txt"
+    generators = "cmake"
+    _cmake = None
+
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    @property
+    def _build_subfolder(self):
+        return "build_subfolder"
+
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
+
+    def configure(self):
+        if self.options.shared:
+            del self.options.fPIC
+
+    def source(self):
+        tools.get(**self.conan_data["sources"][self.version])
+        os.rename(self.name + "-" + self.version, self._source_subfolder)
+
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
+        self._cmake.configure(build_folder=self._build_subfolder)
+        return self._cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build()
+
+    def package(self):
+        self.copy("LICENSE", dst="licenses", src=self._source_subfolder)
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
